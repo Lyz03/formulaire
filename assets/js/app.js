fda1d50ee883ffc8df33636801d64a944249db04
Lyz03 <lizoe@netcourrier.com>
2022-01-14 15:55:19 +0100

    basic form

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,5 @@ const createAccount = document.querySelector('#create_account')
 createAccount.addEventListener("click", function () {
     loginForm.style.display = "none";
     registerForm.style.display = "block";
+    createAccount.style.display = "none";
 })
